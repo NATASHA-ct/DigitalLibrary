e305331f7baaa190e33be26031dd28c42e2a9eee
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-08-23 21:24:05 +0300

    Add redux in category component

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,7 +1,18 @@
-const Category = () => (
-  <button type="button">
-    checkstatus
-  </button>
-);
+import { useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/Categories/categories';
+
+const Category = () => {
+  const dispatch = useDispatch();
+  const checkHandler = () => {
+    dispatch(checkStatus());
+  };
+  return (
+    <div>
+      <button type="button" onClick={checkHandler}>
+        Check status
+      </button>
+    </div>
+  );
+};
 
 export default Category;
